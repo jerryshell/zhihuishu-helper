6a0e3b4e70cc4e9392d406b22dcaff446c557621
Jerry <calljerryli@outlook.com>
2018-05-06 21:03:11 +0800

    Update FuckZhihuishu.js

--- FuckZhihuishu.js
+++ FuckZhihuishu.js
@@ -1,10 +1,20 @@
 /**
  * @author Jerry <calljerryli@outlook.com>
  * @description 智慧树学习助手：自动下一节、自动关闭弹窗、自动 1.5 倍速、自动播放、自动静音
- * @version 1.3.0
+ * @version 1.4.0
  */
 
 var taskTime = 5;
+
+var features = [
+    ClosePopup,
+    NextLesson,
+    SpeedX150,
+    Play,
+    Silent,
+];
+
+var tasksID = new Array();
 
 function ClosePopup() {
     var closeButton = $('.popboxes_close')[0];
@@ -37,9 +47,10 @@ function Play() {
 }
 
 function Silent() {
-    if(!IsSilent()) {
+    if (!IsSilent()) {
         var volumeButton = $('.volumeIcon')[0];
         volumeButton.click();
+        console.log('Silent() success');
     }
 }
 
@@ -48,25 +59,19 @@ function IsSilent() {
 }
 
 function start() {
-    ClosePopup();
-    NextLesson();
-    SpeedX150();
-    Play();
-    Silent();
-    autoClosePopupTaskID = setInterval(ClosePopup, taskTime * 1000);
-    autoNextLessonTaskID = setInterval(NextLesson, taskTime * 1000);
-    autoSpeedX150TaskID = setInterval(SpeedX150, taskTime * 1000);
-    autoPlayTaskID = setInterval(Play, taskTime * 1000);
-    aotuSilentTaskID = setInterval(Silent, taskTime * 1000);
+    for (var i = 0; i < features.length; i++) {
+        features[i]();
+        var taskID = setInterval(features[i], taskTime * 1000);
+        tasksID.push(taskID);
+    }
     console.log('start');
 }
 
 function stop() {
-    clearInterval(autoClosePopupTaskID);
-    clearInterval(autoNextLessonTaskID);
-    clearInterval(autoSpeedX150TaskID);
-    clearInterval(autoPlayTaskID);
-    clearInterval(aotuSilentTaskID);
+    for (var i = 0, len = tasksID.length; i < len; i++) {
+        clearInterval(tasksID.shift());
+        console.log(tasksID);
+    }
     console.log('stop');
 }
 
