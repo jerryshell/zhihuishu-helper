f009b3720b669dea57414a685f0bb0e07b9e4270
Jerry <calljerryli@outlook.com>
2018-12-22 13:37:51 +0800

    重命名 js 文件 & 方法名首字母小写

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 /**
  * 智慧树学习助手
  * @author Jerry <calljerryli@outlook.com>
- * @version 1.4.0
  */
 
 // 任务周期，单位秒
@@ -9,54 +8,54 @@ var taskTime = 5;
 
 // 功能列表，不需要的功能直接在这里删掉即可
 var features = [
-    ClosePopup, // 自动关闭弹窗
-    NextLesson, // 自动进入下一节
-    SpeedX150,  // 自动 1.5 倍速
-    Play,       // 自动播放
-    Silent,     // 自动静音
+    closePopup, // 自动关闭弹窗
+    nextLesson, // 自动进入下一节
+    speedX150,  // 自动 1.5 倍速
+    play,       // 自动播放
+    silent,     // 自动静音
 ];
 
 var tasksID = new Array();
 
-function ClosePopup() {
+function closePopup() {
     var closeButton = $('.popboxes_close')[0];
     if (closeButton != undefined) {
         closeButton.click();
-        console.log('ClosePopup() success');
+        console.log('closePopup() success');
     }
 }
 
-function NextLesson() {
+function nextLesson() {
     var progress = $('div.progressbar_box_tip span')[0];
     if (progress.textContent == "本节视频,累计观看时间『100%』") {
         var nextButton = $('.next_lesson_bg')[0];
         nextButton.click();
-        console.log("NextLesson() success");
+        console.log("nextLesson() success");
     }
 }
 
-function SpeedX150() {
+function speedX150() {
     var speedButton = $('.speedTab15')[0];
     speedButton.click();
 }
 
-function Play() {
+function play() {
     var playButton = $('.playButton')[0];
     if (playButton != undefined) {
         playButton.click();
-        console.log('Play() success');
+        console.log('play() success');
     }
 }
 
-function Silent() {
-    if (!IsSilent()) {
+function silent() {
+    if (!isSilent()) {
         var volumeButton = $('.volumeIcon')[0];
         volumeButton.click();
-        console.log('Silent() success');
+        console.log('silent() success');
     }
 }
 
-function IsSilent() {
+function isSilent() {
     return $('.volumeNone').length != 0;
 }
 
